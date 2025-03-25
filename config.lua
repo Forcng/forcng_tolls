719426a04e600c6e8d23e5d7dcccd328391d7ced
Forcng <164341281+Forcng@users.noreply.github.com>
2025-03-25 02:47:49 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -6,24 +6,6 @@ Config.Booths = {
         distance = 10.0,
         fee = 250
     },
-    {
-        coords = vector3(-2464.5049, -211.2102, 17.3861),
-        distance = 10.0,
-        fee = 250
-    },
-
-    {
-        coords = vector3(-2464.5049, -211.2102, 17.3861),
-        distance = 10.0,
-        fee = 250
-    },
-
-    {
-        coords = vector3(-2464.5049, -211.2102, 17.3861),
-        distance = 10.0,
-        fee = 250
-    },
-
     -- Add More Booths Here Fee Is The Price The Toll Will Charge The Player (Incase you have toll areas that you want to be more expensive or cheaper than others)
     -- {
     --     coords = vector3(-2464.5049, -211.2102, 17.3861),
