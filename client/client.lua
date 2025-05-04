2f24f3af28f1a5ca0f487eab991416d1120269bf
Forcng <164341281+Forcng@users.noreply.github.com>
2025-05-04 14:11:39 -0400

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -7,30 +7,37 @@ CreateThread(function()
         local _IN_VEHICLE = IsPedInAnyVehicle(_PED, false)
 
         if _IN_VEHICLE then
-            local _POS = GetEntityCoords(_PED)
+            local _VEHICLE = GetVehiclePedIsIn(_PED, false)
+            local _IS_DRIVER = GetPedInVehicleSeat(_VEHICLE, -1) == _PED
 
-            for _ZONE_ID, _ZONE in pairs(Config.Booths) do
-                local _DIST = #(_POS - _ZONE.coords)
-                local _IN_ZONE = _DIST < _ZONE.distance
+            if _IS_DRIVER then
+                local _POS = GetEntityCoords(_PED)
 
-                if _IN_ZONE and not _ENTERED_ZONES[_ZONE_ID] then
-                    _ENTERED_ZONES[_ZONE_ID] = true
+                for _ZONE_ID, _ZONE in pairs(Config.Booths) do
+                    local _DIST = #(_POS - _ZONE.coords)
+                    local _IN_ZONE = _DIST < _ZONE.distance
 
-                    -- Shoutout Playboy Carti For The Callback Name - I AM MUSIC!
-                    
-                    lib.callback('forcng:ChargeDemHoesAFee!', false, function(_WAS_CHARGED, _INCOME_SOURCE)
-                        if _WAS_CHARGED then
-                            local _SOURCE_NAME = (_INCOME_SOURCE == "cash" and "pockets") or "bank"
-                            lib.notify({
-                                title = 'Toll Booth',
-                                description = 'You were charged $' .. _ZONE.fee .. ' from your ' .. _SOURCE_NAME .. '.',
-                                type = 'inform',
-                                icon = "fa-solid fa-car",
-                            })
-                        end
-                    end, _ZONE.fee)
+                    if _IN_ZONE and not _ENTERED_ZONES[_ZONE_ID] then
+                        _ENTERED_ZONES[_ZONE_ID] = true
 
-                elseif not _IN_ZONE and _ENTERED_ZONES[_ZONE_ID] then
+                        -- Shoutout Playboy Carti For The Callback Name - I AM MUSIC!
+                        lib.callback('forcng:ChargeDemHoesAFee!', false, function(_WAS_CHARGED, _INCOME_SOURCE)
+                            if _WAS_CHARGED then
+                                local _SOURCE_NAME = (_INCOME_SOURCE == "cash" and "pockets") or "bank"
+                                lib.notify({
+                                    title = 'Toll Booth',
+                                    description = 'You were charged $' .. _ZONE.fee .. ' from your ' .. _SOURCE_NAME .. '.',
+                                    type = 'inform',
+                                    icon = "fa-solid fa-car",
+                                })
+                            end
+                        end, _ZONE.fee)
+                    elseif not _IN_ZONE and _ENTERED_ZONES[_ZONE_ID] then
+                        _ENTERED_ZONES[_ZONE_ID] = false
+                    end
+                end
+            else
+                for _ZONE_ID, _ in pairs(Config.Booths) do
                     _ENTERED_ZONES[_ZONE_ID] = false
                 end
             end
